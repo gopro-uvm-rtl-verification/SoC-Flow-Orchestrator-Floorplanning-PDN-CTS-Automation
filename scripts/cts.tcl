4d2066ed458f50205002209632a0d1a09020f1b5
GoPro_UVM_RTL_Verification <wangyida011103@gmail.com>
2025-09-08 17:39:45 -0400

    Create cts.tcl

---
+++ scripts/cts.tcl
@@ -0,0 +1,2 @@
+puts "## CTS: buffers=$buffers skew=$skew_ps insertion=$insertion_ps useful_skew=$useful_skew"
+# build clock tree; check clock gating; report skew/insertion/power -> reports/clock.rpt
