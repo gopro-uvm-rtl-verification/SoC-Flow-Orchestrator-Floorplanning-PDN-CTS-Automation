26020e8a63690d37e2fe532c53ce25069c43d674
GoPro_UVM_RTL_Verification <wangyida011103@gmail.com>
2025-09-08 17:39:20 -0400

    Create pdn.tcl

---
+++ scripts/pdn.tcl
@@ -0,0 +1,4 @@
+# PDN recipe template
+puts "## PDN: style=$style via_stack=$via_stack decap_ratio=$decap_ratio"
+# foreach strap: layer/pitch/width
+# generate rings/mesh; add vias; place decaps; export reports/ir_em.rpt
