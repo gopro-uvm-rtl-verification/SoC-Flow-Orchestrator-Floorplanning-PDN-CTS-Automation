4597f55c32c549b6c3123398f42348db3191d4a9
GoPro_UVM_RTL_Verification <wangyida011103@gmail.com>
2025-09-09 00:15:28 -0400

    Update pdn.tcl

--- scripts/pdn.tcl
+++ scripts/pdn.tcl
@@ -1,4 +1,5 @@
 # PDN recipe template
-puts "## PDN: style=$style via_stack=$via_stack decap_ratio=$decap_ratio" #这句话是输出PDN配方的注释，包含风格、通孔堆栈和去耦电容比率
+#这句话是输出PDN配方的注释，包含风格、通孔堆栈和去耦电容比率
+puts "## PDN: style=$style via_stack=$via_stack decap_ratio=$decap_ratio" 
 # foreach strap: layer/pitch/width
 # generate rings/mesh; add vias; place decaps; export reports/ir_em.rpt
